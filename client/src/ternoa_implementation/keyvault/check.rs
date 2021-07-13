3fef84e393dde9f37cf2ca248234d0c33e7716c4
Bigna Härdi <bhaerdi@devsgx02.scs-ad.scs.ch>
2021-07-13 11:58:07 +0200

    remove obsolete TODO

--- client/src/ternoa_implementation/keyvault/check.rs
+++ client/src/ternoa_implementation/keyvault/check.rs
@@ -19,7 +19,6 @@ use ternoa_primitives::NFTId;
 
 /// Prints all registered keyvaults and stores all url within a file (one url per line)
 pub fn check(_nft_id: NFTId, _url: &str) -> Result<(), String> {
-    // TODO: check url format?
     // TODO: Task #6, create trusted call
     Ok(())
 }
