9740ad2c68c25c0321c44719390df50029cf4854
Timothée Delabrouille <34384633+tdelabro@users.noreply.github.com>
2021-12-10 11:49:01 +0100

    Retrieve NFT details and store them in enclave
    * Event detection succesfully trigger NOOP ecall
    
    * store and update nft data in enclave sealed storage
    
    * fixed bug caused by unspecified pointer memory size in Enclave.edl + added is_nft_owner ecall
    
    * marko review
    
    Co-authored-by: tdelabro <timothee@capsule-corp.io>

---
+++ core-primitives/sgx/nfts/src/error.rs
@@ -0,0 +1,22 @@
+use derive_more::{Display, From};
+use sgx_types::sgx_status_t;
+use std::prelude::v1::Box;
+
+#[derive(Debug, Display, From)]
+pub enum Error {
+	IO(std::io::Error),
+	Codec(codec::Error),
+	NftNotFound,
+	NftAlreadyExist,
+	Other(Box<dyn std::error::Error>),
+}
+
+pub type Result<T> = core::result::Result<T, Error>;
+
+impl From<Error> for sgx_status_t {
+	/// return sgx_status for top level enclave functions
+	fn from(error: Error) -> sgx_status_t {
+		log::warn!("Transform non-sgx-error into `SGX_ERROR_UNEXPECTED`: {:?}", error);
+		sgx_status_t::SGX_ERROR_UNEXPECTED
+	}
+}
